6c6aa029e4ec977d49a38369225590a4a70c35fc
Molinux <cybercrowbr@gmail.com>
2022-08-26 18:46:43 -0300

    Initial and final sequence IAM code now are now parsed as parameters

--- seq_GisaidToIAM.py
+++ seq_GisaidToIAM.py
@@ -1,17 +1,29 @@
-import csv, os, sys
+import csv, os, argparse
 
 # Renomear csv original para gisaid.csv
 # o arquivo deve ser passado na linha de comando
-# python3 seq_GisaidToIAM.py <arquivo>
+# python3 seq_GisaidToIAM.py -f <arquivo>
 
-codInicial = "IAM11355"
-codFinal="IAM11544"
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", required=True)
+parser.add_argument("--initial", required=True)
+parser.add_argument("--final", required=True)
+args = parser.parse_args()
+new_name = r"gisaid.csv"
+os.rename(args.file, new_name)
+
+# TODO: Automatizar a passagem de codInicial e codFinal
+# codInicial = "IAM11355"
+# codFinal="IAM11544"
+codInicial = args.initial
+codFinal= args.final
 cod_IAM = codInicial
 line_count = 0
 gisaid_codes = []
 
 with open('gisaid.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
+    # csv_reader = csv.reader(csv_file, delimiter=",")
+    csv_reader = csv.reader(csv_file, delimiter="\t")
     for row in csv_reader:
         if line_count == 0:
             print(f'Column names are {", ".join(row)}')
@@ -40,8 +52,13 @@ with open('gisaid.csv') as csv_file:
 
 with open('GisaidToIAM.csv','w') as file:
    for i in gisaid_codes:
-    file.writelines(i)
-    file.writelines('\n')
+        file.writelines(i)
+        file.writelines('\n')
+
+print(" ")
+print('GisaidToIAM.csv gerado com sucesso')
+print(" ")
+
 
 # print(" ")
 # print("========== GISAID CODES ==========")
